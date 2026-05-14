ed21919455a0e11e199f6e9419d92959309531a3
ajrp_arbejdspc <ajrp@tec.dk>
2026-05-14 10:26:09 +0200

    offline windows version

---
+++ startFromWin.bat
@@ -0,0 +1,12 @@
+@echo off
+:: 1. Skift arbejdsmappe til der hvor batch-filen er gemt
+cd /d "%~dp0"
+
+:: 2. Start dit program i baggrunden (erstat med 'min-hjemmeside.exe' hvis det er en exe-fil)
+start "" "compileDenoWin.exe"
+
+:: 3. Vent 2 sekunder på at Deno-serveren starter
+timeout /t 2 /nobreak >nul
+
+:: 4. Åbn standardbrowseren på localhost
+start http://localhost:8000
